545aa170f470e876db574d98c1025e58d0f35cbf
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-06-26 14:13:29 -0400

    implements routes logic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/whatever" element={<div>Route 1</div>}></Route>
+        <Route path="/about" element={<div>Route 2</div>}></Route>
+        <Route path="/" element={<div>Route 3</div>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
