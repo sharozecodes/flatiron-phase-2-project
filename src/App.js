229b6138e1fa4d2f43efdb44df195db33d647b34
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-06-27 18:59:18 -0400

    adds basic navbar css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import NavBar from "./Components/NavBar";
-import Whatever from "./Components/Whatever";
-import Home from "./Components/Home";
-import About from "./Components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/whatever">
-          <Whatever />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Routes>
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">
+        Horoscope App
+      </Link>
+      <ul className="navbar-links">
+        <li className="navbar-link-item">
           <Link to="/zodiac-signs">Zodiac Signs</Link>
         </li>
-        <li>
+        <li className="navbar-link-item">
           <Link to="/horoscope">Horoscope</Link>
         </li>
       </ul>
