9691e9b4e2b5b54c85130050972e0b6de8d74c6a
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-01 19:27:03 -0400

    removes unused files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
 import Home from "./Pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Zodiac from "./Components/Zodiac";
 import UserList from "./Components/UserList";
-//import Horoscope from "./Components/Horoscope";
 import Horoscope from "./Components/Horoscope";
 
 const router = createBrowserRouter([
