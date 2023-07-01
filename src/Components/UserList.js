5c76de3c020cc96e50b74e0a6501020a746842db
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-01 13:59:06 -0400

    deletes clearUser

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-//import ClearUser from "./ClearUser";
 //import { useParams } from "react-router-dom";
 
 const UserList = () => {
