440f7cf90068135abde85fb83c4170b4dc93deb8
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-01 01:19:55 -0400

    implements back buttons

--- src/Components/Horoscope.js
+++ src/Components/Horoscope.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const Horoscope = () => {
+const Horoscope = ({ name }) => {
+  const navigate = useNavigate();
   const { zodiacSign } = useParams();
   const [zodiacSigns, setZodiacSigns] = useState([]);
 
@@ -16,11 +18,13 @@ const Horoscope = () => {
   const favoriteFood = zodiacSigns.find(
     (zodiac) => zodiac.name === zodiacSign
   )?.favoriteFood;
+  console.log(name);
 
   return (
     <div>
       <h2>{zodiacSign}</h2>
       <h1>{favoriteFood}</h1>
+      <button onClick={() => navigate("/")}>Back</button>
     </div>
   );
 };
