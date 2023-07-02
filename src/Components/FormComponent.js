38fcde4b70375e6050ff1ac2e7666fcabf51f9cc
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-02 14:02:03 -0400

    code cleanup

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Button, Stack } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const FormComponent = ({ name, dateOfBirth, setName, setDateOfBirth }) => {
   const navigate = useNavigate();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Zodiac from "./Components/Zodiac";
